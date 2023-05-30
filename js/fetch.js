e175826ffc0919224649ce23dcae842362028e6c
Zasha <0925315@hr.nl>
2023-05-30 22:36:54 +0200

    testing

--- js/fetch.js
+++ js/fetch.js
@@ -1,5 +1,7 @@
 async function getSheetData() {
-    const response = await fetch("https://sheets.googleapis.com/v4/spreadsheets/{spreadsheetId}");
+    const response = await fetch("https://sheets.googleapis.com/v4/spreadsheets/17LbbqsemrAKyXvfnGdZM2Aox32umyBAFhWvUwzK_Cw4");
     const jsonData = await response.json();
     console.log(jsonData);
 }
+
+getSheetData()
