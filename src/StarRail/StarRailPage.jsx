7e4654624893e107629cf7de21d50c97c12316bd
Wilco Ram <wilcoram.spectra@gmail.com>
2023-06-05 19:15:23 +0200

    key gone

--- src/StarRail/StarRailPage.jsx
+++ src/StarRail/StarRailPage.jsx
@@ -7,7 +7,7 @@ const MastersheetData = () => {
     const [data, setData] = useState(null);
 
     useEffect(() => {
-        fetch("https://sheets.googleapis.com/v4/spreadsheets/17LbbqsemrAKyXvfnGdZM2Aox32umyBAFhWvUwzK_Cw4?key=AIzaSyB0qY5nz7IRrgsuOWi-tHGeMagRbsvUpNU")
+        fetch("https://sheets.googleapis.com/v4/spreadsheets/17LbbqsemrAKyXvfnGdZM2Aox32umyBAFhWvUwzK_Cw4?key=")
             .then((res) => res.json())
             .then((data) => setData(data));
     }, []);
