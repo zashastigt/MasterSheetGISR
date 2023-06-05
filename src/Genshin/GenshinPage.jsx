190f96a01a22b7df58c35f53599ef4fca7cf3aa0
Rick <rickgroenevelt2000@gmail.com>
2023-06-05 20:23:50 +0200

    Cleanup of Genshinpage.jsx

--- src/Genshin/GenshinPage.jsx
+++ src/Genshin/GenshinPage.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { useState, useRef, useEffect } from 'react'
 import './GenshinPage.css'
+import elements from '../data/elements.json'
+import weapons from '../data/weapons.json'
 
 function SearchBar() {
 
@@ -29,76 +31,27 @@ function SearchBar() {
     )
 }
 
+function getFilterButton(key, label, url){
+    return (
+        <label key={key} htmlFor={`#${key}`}>
+            <input type="checkbox" value={key} id={key} />
+            <img className="element" src={url} />
+            <span>{label}</span>
+        </label>
+    )
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <div className="container">
             <SearchBar />
             <div className="filters">
-                <div className="elements">
-                    <label>
-                        <input type="checkbox" value="Pyro" id="Pyro" />
-                        <img className="element" src="../assets/pyro.svg" />
-                        Pyro
-                    </label>
-                    <label>
-                        <input type="checkbox" value="Cryo" id="Cryo" />
-                        <img className="element" src="../assets/cryo.svg"/>
-                        Cryo
-                    </label>
-                    <label>
-                        <input type="checkbox" value="Hydro" id="Hydro" />
-                        <img className="element" src="../assets/hydro.svg" />
-                        Hydro
-                    </label>
-                    <label>
-                        <input type="checkbox" value="Electro" id="Electro" />
-                        <img className="element" src="../assets/electro.svg" />
-                        Electro
-                    </label>
-                    <label>
-                        <input type="checkbox" value="Anemo" id="Anemo" />
-                        <img className="element" src="../assets/anemo.svg" />
-                        Anemo
-                    </label>
-                    <label>
-                        <input type="checkbox" value="Geo" id="Geo" />
-                        <img className="element" src="../assets/geo.svg" />
-                        Geo
-                    </label>
-                    <label>
-                        <input type="checkbox" value="Dendro" id="Dendro" />
-                        <img className="element" src="../assets/dendro.svg" />
-                        Dendro
-                    </label>
-                </div>
-                <div className="weapons">
-                    <label>
-                        <input type="checkbox" value="Sword" id="Sword" />
-                        <img className="weapon" src="https://api.ambr.top/assets/UI/UI_GachaTypeIcon_Sword.png" />
-                        Sword
-                    </label>
-                    <label>
-                        <input type="checkbox" value="Claymore" id="Claymore" />
-                        <img className="weapon" src="https://api.ambr.top/assets/UI/UI_GachaTypeIcon_Claymore.png" />
-                        Claymore
-                    </label>
-                    <label>
-                        <input type="checkbox" value="Bow" id="Bow" />
-                        <img className="weapon" src="https://api.ambr.top/assets/UI/UI_GachaTypeIcon_Bow.png" />
-                        Bow
-                    </label>
-                    <label>
-                        <input type="checkbox" value="Polearm" id="Polearm" />
-                        <img className="weapon" src="https://api.ambr.top/assets/UI/UI_GachaTypeIcon_Pole.png" />
-                        Polearm
-                    </label>
-                    <label>
-                        <input type="checkbox" value="Catalyst" id="Catalyst" />
-                        <img className="weapon" src="https://api.ambr.top/assets/UI/UI_GachaTypeIcon_Catalyst.png" />
-                        Catalyst
-                    </label>
-                </div>
+                <ul className="elements">
+                    {Object.keys(elements).map((k)=>getFilterButton(k, elements[k].label, `../assets/${elements[k].urlKey}.svg`))}
+                </ul>
+                <ul className="weapons">
+                    {Object.keys(weapons).map((k)=>getFilterButton(k, weapons[k].label, `https://api.ambr.top/assets/UI/UI_GachaTypeIcon_${weapons[k].urlKey}.png`))}
+                </ul>
             </div>
             <div className="characters">
                 <div className="character"></div>
