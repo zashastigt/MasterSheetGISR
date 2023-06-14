0c62e097f83dd3b8d484e127b1f0642de5035f42
Zasha <0925315@hr.nl>
2023-06-14 16:35:54 +0200

    werkt nu voor alle pages

--- src/data/postData.js
+++ src/data/postData.js
@@ -1,5 +1,5 @@
 export default async function postData(data = {}) {
-    const response = await fetch("https://script.google.com/macros/s/AKfycbxljRoACHe_iRNR1W7WsLZ90WLtV6SzqyFutvkmZ5di6YktMINcvPIes9_kpnQhCF2z0w/exec", {
+    await fetch("https://script.google.com/macros/s/AKfycbxljRoACHe_iRNR1W7WsLZ90WLtV6SzqyFutvkmZ5di6YktMINcvPIes9_kpnQhCF2z0w/exec", {
         method: "POST",
         headers: {
             "Content-Type": "text/plain",
