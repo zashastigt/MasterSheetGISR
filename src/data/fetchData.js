399ac180f949350b85919059754b22bcc15bb2dd
Zasha <0925315@hr.nl>
2023-06-14 16:35:42 +0200

    werkt nu voor alle pages

--- src/data/fetchData.js
+++ src/data/fetchData.js
@@ -1,4 +1,4 @@
 export async function getSheetDataJson() {
-    return await fetch("https://script.google.com/macros/s/AKfycbwkETE2cbx-BfhO2kO9one5zRZFLVvR7_zaLwZhv4TYH1s-qvy_TrnFWZnVMM7do5Em8Q/exec")
+    return await fetch("https://script.google.com/macros/s/AKfycbxljRoACHe_iRNR1W7WsLZ90WLtV6SzqyFutvkmZ5di6YktMINcvPIes9_kpnQhCF2z0w/exec")
     .then(response => { return response.json() });
 }
