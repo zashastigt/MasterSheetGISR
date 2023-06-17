192534cc88ac5d15c02fe79402b2fcead22d412c
Zasha <0925315@hr.nl>
2023-06-17 16:21:37 +0200

    begonnen met pity

--- src/Universal/pityBox/pityBox.jsx
+++ src/Universal/pityBox/pityBox.jsx
@@ -2,94 +2,141 @@ import React, {useState} from "react";
 import './pityBox.css'
 
 export default function PityBox() {
+    const [selectedPerson, setSelectedPerson] = useState('')
+    const [regular4Pity, setRegular4Pity] = useState(0)
+    const [weapon4Pity, setWeapon4Pity] = useState(0)
+    const [character4Pity, setCharacter4Pity] = useState(0)
+
     const testPity = [
         {
             Name: 'Zasha',
             Regular: 51,
             Weapon: 0,
             Character: 75,
-            Guarantee: true
+            Guarantee: 'Yes'
         },
         {
             Name: 'Wilco',
             Regular: 51,
             Weapon: 0,
             Character: 14,
-            Guarantee: false
+            Guarantee: 'No'
         },
         {
             Name: 'Wilfred',
             Regular: 50,
             Weapon: 0,
             Character: 69,
-            Guarantee: true
+            Guarantee: 'Yes'
         },
         {
             Name: 'Rick',
             Regular: 0,
             Weapon: 0,
             Character: 0,
-            Guarantee: false
+            Guarantee: 'No'
         }
     ]
 
 
     function Pity({pity}) {
-        const [regular4Pity, setRegular4Pity] = useState(0)
-        const [weapon4Pity, setWeapon4Pity] = useState(0)
-        const [character4Pity, setCharacter4Pity] = useState(0)
 
         return (
-        <div className={'pityInfo'}>
+        <div className={'pityInfo'} onClick={() => setSelectedPerson(pity)}>
             <div className={'pityName'}>{pity.Name}</div>
             <div className={'pityPity'}>
                 <div className={'pityColumn'}>
                     <div className={'pityBarColor pity5Color'}></div>
-                    <div>{pity.Regular}<button className={'pityButton'}>R</button></div>
-                    <div>{pity.Weapon}<button className={'pityButton'}>R</button></div>
-                    <div>{pity.Character}<button className={'pityButton'}>R</button></div>
+                    <div>{pity.Regular}</div>
+                    <div>{pity.Weapon}</div>
+                    <div>{pity.Character}</div>
                     <div>{pity.Guarantee ? 'yes' : 'no'}</div>
                 </div>
                 <div className={'pityColumn'}>
                     <div className={'pityBarColor pity4Color'}></div>
-                    <div>{regular4Pity}<button className={'pityButton'}>R</button></div>
-                    <div>{weapon4Pity}<button className={'pityButton'}>R</button></div>
-                    <div>{character4Pity}<button className={'pityButton'}>R</button></div>
-                </div>
-                <div className={'pityColumn'}>
-                    <div className={'pityBarColor pityAColor'}></div>
-                    <div>
-                        <button className={'pityButton'}>+</button>
-                    </div>
-                    <div>
-                        <button className={'pityButton'}>+</button>
-                    </div>
-                    <div>
-                        <button className={'pityButton'}>+</button>
-                    </div>
-                    <div>
-                        <button className={'pityButton'}>S</button>
-                    </div>
+                    <div>{regular4Pity}</div>
+                    <div>{weapon4Pity}</div>
+                    <div>{character4Pity}</div>
                 </div>
             </div>
         </div>
         )
     }
 
+    function PityInput() {
+        return (
+            <div className={'pityInput'}>
+                <div>
+                    <div>{selectedPerson.Name}</div>
+                    <div className={'star5'}>5 Star</div>
+                    <div className={'star4'}>4 Star</div>
+                </div>
+                <div className={'pityInputRow'}>
+                    <div className={'pityInputCell'}>
+                        <div>Normal</div>
+                        <div className={'star5'}>{selectedPerson.Regular}</div>
+                        <div className={'star4'}>{regular4Pity}</div>
+                        <button className={'button'}>+1</button>
+                        <div>
+                            <button className={'reset star4'}>4<img alt={'reset'} src={'/assets/reset.png'}></img></button>
+                            <button className={'reset star5'}>5<img alt={'reset'} src={'/assets/reset.png'}></img></button>
+                        </div>
+
+                    </div>
+                    <div className={'pityInputCell'}>
+                        <div>Weapon</div>
+                        <div className={'star5'}>{selectedPerson.Weapon}</div>
+                        <div className={'star4'}>{weapon4Pity}</div>
+                        <button className={'button'}>+1</button>
+                        <div>
+                            <button className={'reset star4'}>4<img alt={'reset'} src={'/assets/reset.png'}></img></button>
+                            <button className={'reset star5'}>5<img alt={'reset'} src={'/assets/reset.png'}></img></button>
+                        </div>
+                    </div>
+                    <div className={'pityInputCell'}>
+                        <div>Character</div>
+                        <div className={'star5'}>{selectedPerson.Character}</div>
+                        <div className={'star4'}>{character4Pity}</div>
+                        <button className={'button'}>+1</button>
+                        <div>
+                            <button className={'reset star4'}>4<img alt={'reset'} src={'/assets/reset.png'}></img></button>
+                            <button className={'reset star5'}>5<img alt={'reset'} src={'/assets/reset.png'}></img></button>
+                        </div>
+                    </div>
+                    <div className={'pityInputCell'}>
+                        <div>50/50</div>
+                        <div className={'star5'}>{selectedPerson.Guarantee}</div>
+                    </div>
+                </div>
+
+                <button className={'close button'} onClick={() => setSelectedPerson('')}>X</button>
+            </div>
+        )
+    }
+
     return (
         <div className={'pityBox'}>
+
             <div className={'pityContainer'}>
-                <div className={'pityColumn'}>
-                    <div>Which</div>
-                    <div className={'pityBarColor pityNoColor'}></div>
-                    <div>Normal</div>
-                    <div>Weapon</div>
-                    <div>Event</div>
-                    <div>50/50</div>
-                </div>
-                {testPity.map(pity => {
-                    return <Pity pity={pity} />
-                })}
+
+                {selectedPerson === '' ?
+                    <>
+                        <div className={'pityInputCell'}>
+                            <div>Who</div>
+                            <div className={'pityBarColor pityNoColor'}></div>
+                            <div>Normal</div>
+                            <div>Weapon</div>
+                            <div>Event</div>
+                            <div>50/50</div>
+                        </div>
+                        {testPity.map(pity => {
+                            return <Pity pity={pity}/>
+                        })}
+                    </>
+
+                    :
+                    <PityInput />
+                }
 
             </div>
             <div className={'pityBoxName'}>Pity</div>
