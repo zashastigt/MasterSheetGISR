10d0c8192149c11b41e6f9a378b3edf2712732eb
Wilco Ram <wilcoram.spectra@gmail.com>
2023-11-14 16:54:06 +0100

    Updated Sheet API links

--- src/data/postData.js
+++ src/data/postData.js
@@ -1,7 +1,7 @@
 export default async function postData(data = {}) {
     const key = (`; ${localStorage.getItem('Key')}`).split(`; `).pop().split(';')[0];
 
-    await fetch(`https://script.google.com/macros/s/AKfycbxMeQkR22stgbD7T0KwUL4wjolvGCkIUfdaENFoGP9YZ9arXYg_SxO4sxzdbfiKSBpPcg/exec?cookie=${key}`, {
+    await fetch(`https://script.google.com/macros/s/AKfycbxqrYBnLWkDYvXtuL-jMN-odVCInTvDaGm6gPaXRNcElEkMZcNLFx2z8duQTYavMovdjA/exec?cookie=${key}`, {
         method: "POST",
         headers: {
             "Content-Type": "text/plain",
