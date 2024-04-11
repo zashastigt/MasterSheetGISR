54e261e326885ce68af348dd7401d64a6fceecfd
Zasha <0925315@hr.nl>
2024-04-11 22:56:28 +0200

    google link update

--- src/data/postData.js
+++ src/data/postData.js
@@ -1,7 +1,7 @@
 export default async function postData(data = {}) {
     const key = (`; ${localStorage.getItem('Key')}`).split(`; `).pop().split(';')[0];
 
-    await fetch(`https://script.google.com/macros/s/AKfycbxqrYBnLWkDYvXtuL-jMN-odVCInTvDaGm6gPaXRNcElEkMZcNLFx2z8duQTYavMovdjA/exec?cookie=${key}`, {
+    await fetch(`https://script.google.com/macros/s/AKfycbwSN9W5hRlSUTGJU3uD8_3hkScRFzn5RwnuRDLd_Gwibh7_Mdvda4lTreJxdmt030BsCg/exec?cookie=${key}`, {
         method: "POST",
         headers: {
             "Content-Type": "text/plain",
