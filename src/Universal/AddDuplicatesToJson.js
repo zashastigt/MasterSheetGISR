8ceffb494a504db3a097fbe32b66b170c1264aee
Wilco Ram <wilcoram.spectra@gmail.com>
2024-04-26 18:31:14 +0200

    Removed unnecessary if statement which caused unreachable sorting

--- src/Universal/AddDuplicatesToJson.js
+++ src/Universal/AddDuplicatesToJson.js
@@ -1,7 +1,6 @@
 import postData from "../data/postData.js";
 
 function AddDuplicatesToJson(data, sheetData, pityPlayers) {
-    let newItem = false
     let newData = data.map(key => {
         if (sheetData.Characters[key.name] !== undefined && key.name == sheetData.Characters[key.name].Name) return { ...key, CE: sheetData.Characters[key.name].CE }
         else if (key.name.includes('Trailblazer')) return { ...key, CE: sheetData.Characters[`${key.name} ${key.types.combatType.Element}`].CE }
@@ -24,20 +23,18 @@ function AddDuplicatesToJson(data, sheetData, pityPlayers) {
         postData(postItem)
 
         const newCE = {}
-        newItem = true
         Object.values(pityPlayers).map((name) => newCE[name.Name] = "")
 
         return { ...key, CE: newCE }
     })
 
-    if (newItem) {
-        newData.sort((a, b) => {
-            if (a.rank !== b.rank) return (a.rank > b.rank) ? -1 : 1;
-            if (a.types.combatType !== undefined && a.types.combatType.Element !== b.types.combatType.Element) return (a.types.combatType.Element < b.types.combatType.Element) ? -1 : 1;
-            else if (a.types.pathType !== undefined && a.types.pathType.Path !== b.types.pathType.Path) return (a.types.pathType.Path < b.types.pathType.Path) ? -1 : 1;
-            return (a.name < b.name) ? -1 : 1;
-        })
-    }
+    newData.sort((a, b) => {
+        if (a.rank !== b.rank) return (a.rank > b.rank) ? -1 : 1;
+        if (a.types.combatType !== undefined && a.types.combatType.Element !== b.types.combatType.Element) return (a.types.combatType.Element < b.types.combatType.Element) ? -1 : 1;
+        else if (a.types.pathType !== undefined && a.types.pathType.Path !== b.types.pathType.Path) return (a.types.pathType.Path < b.types.pathType.Path) ? -1 : 1;
+        return (a.name < b.name) ? -1 : 1;
+    })
+
     return newData
 }
 
